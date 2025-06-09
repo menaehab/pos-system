104cc2805fe13144ac87303f430cfbb46fa5e44c
Mena Ehab <127118141+menaehab@users.noreply.github.com>
2025-06-09 10:33:23 +0300

    feat: setup login page

--- routes/web.php
+++ routes/web.php
@@ -1,23 +1,35 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 use Livewire\Volt\Volt;
+use App\Livewire\Auth\Login;
+use App\Livewire\Actions\Logout;
+use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 })->name('home');
 
-Route::view('dashboard', 'dashboard')
-    ->middleware(['auth', 'verified'])
-    ->name('dashboard');
-
-Route::middleware(['auth'])->group(function () {
-    Route::redirect('settings', 'settings/profile');
-
-    Volt::route('settings/profile', 'settings.profile')->name('settings.profile');
-    Volt::route('settings/password', 'settings.password')->name('settings.password');
-    Volt::route('settings/appearance', 'settings.appearance')->name('settings.appearance');
-});
+// logout
+Route::post('logout', Logout::class)->name('logout');
 
 
-require __DIR__.'/auth.php';
+Route::get('login', Login::class)->name('login');
+
+
+
+
+
+// Route::view('dashboard', 'dashboard')
+//     ->middleware(['auth', 'verified'])
+//     ->name('dashboard');
+
+// Route::middleware(['auth'])->group(function () {
+//     Route::redirect('settings', 'settings/profile');
+
+//     Volt::route('settings/profile', 'settings.profile')->name('settings.profile');
+//     Volt::route('settings/password', 'settings.password')->name('settings.password');
+//     Volt::route('settings/appearance', 'settings.appearance')->name('settings.appearance');
+// });
+
+
+// require __DIR__.'/auth.php';
